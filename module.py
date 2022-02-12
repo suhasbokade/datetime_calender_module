3110d2753ff08ae3dad296cf290c73630d191f00
Suhas Bokade <99481658+suhasbokade@users.noreply.github.com>
2022-02-12 09:38:37 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,30 @@
+import math
+
+print(math.pi)
+
+#######################
+
+import datetime
+
+datetime_today = datetime.datetime.now()
+print(datetime_today)
+
+###############
+
+import string
+
+print(int)
+
+#############
+
+import statistics
+
+print(statistics.median([2, 5, 6, 9]))
+
+###########
+
+import calendar
+
+yy = 2021
+mm = 9
+print(calendar.month(yy, mm))
